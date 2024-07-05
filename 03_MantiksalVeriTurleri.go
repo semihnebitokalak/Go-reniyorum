0208919caa951e091f3f88892a30e3e35717d90f
GitHub Kullanıcı Adınız <GitHub E-posta Adresiniz>
2024-07-06 02:12:07 +0300

    Mantıksal veri türü olan Boolean'ı inceledik

--- 03_MantiksalVeriTurleri.go
+++ 03_MantiksalVeriTurleri.go
@@ -7,16 +7,20 @@ package main
 import "fmt"
 
 func main() {
-	var durum bool
+	var durum1 bool
+	var durum2 bool
 	var metin1 = "Engin"
 	var metin2 = "Ahmet"
+	var metin3 = "Engin"
 	// Buradaki iki eşittir mantıksal operatördür ve iki durumunun birbirine eşit olup olmamasını kontrol eder.
 	// Mantıksal operatörlerde != ifadesi de yer alır ve iki değer eşit değilse anlamı katar.
-	durum = metin1 == metin2
+	durum1 = metin1 == metin2
+	durum2 = metin1 == metin3
 
-	fmt.Println(durum)
+	fmt.Println(durum1)
 	// Eğer ünlem koyarsak aşağıdaki kod bloğundaki gibi verilen değerin tersini yazdıracaktır.
-	fmt.Println(!durum)
+	fmt.Println(!durum1)
 	// Ayrıca alt satıra inmek için \n kullanabiliriz...
+	fmt.Println(durum2)
 
 }
