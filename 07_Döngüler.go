919190dfdf1bb02cc2097630a63aa5f6ef67f705
GitHub Kullanıcı Adınız <GitHub E-posta Adresiniz>
2024-07-07 02:35:19 +0300

    Döngüleri anlamaya çalıştık

---
+++ 07_Döngüler.go
@@ -0,0 +1,31 @@
+// Bu dersimizde for döngüsünü göreceğiz...
+// for döngüsü tekrar etme verilen koşula kadar gidip istediğimiz işlemi yaptırma demektir...
+
+package main
+
+import "fmt"
+
+func main() {
+	var metin string = "Merhaba Dünya!"
+	fmt.Println(metin)
+	fmt.Println(metin)
+	fmt.Println(metin)
+	fmt.Println(metin)
+	fmt.Println(metin)
+	fmt.Println(metin)
+	fmt.Println(metin)
+	// Bu şekilde birçok kez ekrana yazdırabiliriz.
+	// Bunu döngü ile tek bir işlemde yapabiliriz...
+	// Şimdi for döngüsünün başlangıç değeri için i diye bir değişken tanımlıyoruz...
+	i := 1
+	// infinite loop
+	for i <= 5 {
+		fmt.Println(metin)
+		// yukarıdaki şekilde çalıştırırsanız sonsuz döngüye girecektir çünkü i değeri sabit.
+		i = i + 1
+	}
+	// for döngüsünün asıl kullanımı bu şekildedir.
+	for i := 0; i < 4; i++ {
+		fmt.Println(metin)
+	}
+}
