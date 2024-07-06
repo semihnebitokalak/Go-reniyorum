219646527b22c4e129571ba075364503a0f511df
GitHub Kullanıcı Adınız <GitHub E-posta Adresiniz>
2024-07-06 16:12:35 +0300

    Koşul ve şart kavramlarını anladık

---
+++ 05_Koşul_Sart.go
@@ -0,0 +1,10 @@
+// Bu dersimizde şart bloklarıyla çalışacağız...
+// Şart bloklarını anlamaya çalışacağız...
+// Şimdi kendimize koşul klosörü oluşturalım ve işlemlerimizi orada yapalım...
+package main
+
+import "goLesson/conditionals"
+
+func main() {
+	conditionals.Demo1()
+}
